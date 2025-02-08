412269a010e2986b6d48d0e341690d52a0dc93e9
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2025-02-08 20:19:38 +0000

    create initial matrix multiplication folder and file

---
+++ Matrix-Multiplication/matrix_multiplication.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class MatrixMultiplication {
+public:
+    void computeMatrixMultiplication() {
+        std::cout << "Hello, World!" << std::endl;
+    }
+};
+
+int main() {
+    MatrixMultiplication mat;
+    mat.computeMatrixMultiplication()
+    return 0;
+}
+
