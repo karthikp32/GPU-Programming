ce20d4d6e3714d10aa256abce139665b73ef2faf
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2025-02-09 17:39:14 -0500

    Update matrix_multiplication.cpp
     generated initial code to do matrix multiplication

--- Matrix-Multiplication/matrix_multiplication.cpp
+++ Matrix-Multiplication/matrix_multiplication.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
+#include <cuda_runtime.h>
+
 
 class MatrixMultiplication {
 public:
-    void computeMatrixMultiplication() {
-        std::cout << "Hello, World!" << std::endl;
+    
+    __global__ void matrix_multiply((int a[1024][1024], int b[1024][1024], int result[1024][1024]) {
+        
     }
 };
 
-int main() {
-    MatrixMultiplication mat;
-    mat.computeMatrixMultiplication()
-    return 0;
+    void fillMatrix(int matrix[1024][1024]) {
+    for (int i = 0; i < 1024; i++) {
+        for (int j = 0; j < 1024; j++) {
+            matrix[i][j] = rand() % 100; // Random values between 0-99
+        }
+    }
 }
 
+
+int main() {
+    MatrixMultiplication mat;
+    int[][] matA = new int[1024][1024];
+    int[][] matB = new int[1024][1024];
+    int[][] result = {0};
+    
+    mat.fillMatrix(matA);
+    mat.fillMatrix(matB);
+
+    mat.matrix_multiply()
+    return 0;
+}
